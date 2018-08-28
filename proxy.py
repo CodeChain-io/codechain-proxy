04e220ba9f89addcfd5de6286cdfe12cc10f1f8e
Hyunsik Jeong <jhs7jhs@naver.com>
2018-08-28 10:06:43 +0900

    Change the error response into the JSON-RPC format
    See https://www.jsonrpc.org/historical/json-rpc-over-http.html
    for the detail of JSON-RPC over HTTP.

--- proxy.py
+++ proxy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 from flask import Flask
-from flask import request, abort
+from flask import request
 import requests
 
 if __name__ == '__main__':
@@ -14,6 +14,8 @@ if __name__ == '__main__':
                         help='binding port')
     parser.add_argument('--forward', type=int, default=8080,
                         help='port to forward (deafult: 8080)')
+    parser.add_argument('--debug', action='store_true',
+                        help='enable debug mode')
 
     args = parser.parse_args()
 
@@ -22,20 +24,43 @@ if __name__ == '__main__':
         app.whitelist = set(line.rstrip() for line in f)
     app.forward = args.forward
 
+    def error_msg(code, msg, id):
+        id = "null" if id is None else id
+        return '{{"jsonrpc":"2.0","error":{{"code":{},"message":"{}"}},"id":{}}}\n'.format(code, msg, id)
+
+    def parse_error():
+        return error_msg(-32700, "Parse error", None), 500
+
+    def invalid_request():
+        return error_msg(-32600, "Invalid request", None), 400
+
+    def method_not_found(id):
+        return error_msg(-32601, "Method not found", id), 404
+
+    def internal_error(id):
+        return error_msg(-32603, "Internal error", id), 500
+
     @app.route('/', methods = ['POST'])
     def proxy():
-        if not request.is_json: # Invalid JSON Format
-            abort(404)
-        content = request.get_json()
+        raddr = request.remote_addr
+        if not request.is_json:
+            return invalid_request()
+
+        content = request.get_json(silent=True)
+        if content is None:
+            return parse_error()
+
+        if 'id' not in content:
+            return ""
 
         if 'method' in content and content['method'] in app.whitelist:
             try:
                 forward_addr = 'http://localhost:{}'.format(app.forward)
                 r = requests.post(forward_addr, json=content)
                 return r.text
-            except: # Failed to get value from the rpc server
-                abort(404)
-        else: # Filtered.
-            abort(404)
+            except Exception as e:
+                return internal_error(content['id'])
+        elif 'method' in content:
+            return method_not_found(content['id'])
 
-    app.run(host=args.bind, port=args.port)
+    app.run(host=args.bind, port=args.port, debug=args.debug)
