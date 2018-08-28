f33ff99ba6df5d7b235cb1a29502ebde5551fef1
Hyunsik Jeong <jhs7jhs@naver.com>
2018-08-28 10:08:51 +0900

    Add logging
    Now RotatingFileHandler is added to the Flask app.
    It logs infos and errors from the app.

--- proxy.py
+++ proxy.py
@@ -3,6 +3,8 @@ import argparse
 from flask import Flask
 from flask import request
 import requests
+import logging
+from logging.handlers import RotatingFileHandler
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -14,6 +16,8 @@ if __name__ == '__main__':
                         help='binding port')
     parser.add_argument('--forward', type=int, default=8080,
                         help='port to forward (deafult: 8080)')
+    parser.add_argument('--log', default='proxy.log',
+                        help='log file path (default: \"proxy.log\")')
     parser.add_argument('--debug', action='store_true',
                         help='enable debug mode')
 
@@ -23,6 +27,16 @@ if __name__ == '__main__':
     with open(args.whitelist) as f:
         app.whitelist = set(line.rstrip() for line in f)
     app.forward = args.forward
+
+    formatter = logging.Formatter('[%(asctime)s] [%(levelname)s in %(module)s] %(message)s')
+    handler = RotatingFileHandler(args.log, maxBytes=0x100000, backupCount=3)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.INFO)
+
+    def log(address, message, level=logging.INFO):
+        app.logger.log(level, '[{}] {}'.format(address, message))
 
     def error_msg(code, msg, id):
         id = "null" if id is None else id
@@ -44,10 +58,12 @@ if __name__ == '__main__':
     def proxy():
         raddr = request.remote_addr
         if not request.is_json:
+            log(raddr, 'Received request is not JSON')
             return invalid_request()
 
         content = request.get_json(silent=True)
         if content is None:
+            log(raddr, 'Failed to parse JSON request: {}'.format(request.data))
             return parse_error()
 
         if 'id' not in content:
@@ -57,10 +73,13 @@ if __name__ == '__main__':
             try:
                 forward_addr = 'http://localhost:{}'.format(app.forward)
                 r = requests.post(forward_addr, json=content)
+                log(raddr, 'Successfully forwarded {} / Response {}'.format(content, r.text.strip()))
                 return r.text
             except Exception as e:
+                log(raddr, 'Failed to receive the response from the server: {}'.format(e))
                 return internal_error(content['id'])
         elif 'method' in content:
+            log(raddr, 'Filtered {}: {}'.format(content['method'], content))
             return method_not_found(content['id'])
 
     app.run(host=args.bind, port=args.port, debug=args.debug)
